17ff5641314ece8a0561aa8a1b91c44ffadb4f8b
Smiteshbhore21 <smiteshbhore23@gmail.com>
2024-08-13 00:17:45 +0530

    Added Funtion for Addition on Two Numbers

--- Main.js
+++ Main.js
@@ -3,6 +3,10 @@ class Main {
         console.log("Welcome to TechLathe IT Solutions");
 
     }
+    static addOfTwoNumbers(a,b){
+        console.log(a+b);
+    }
 }
 
 Main.printMessage();
+Main.addOfTwoNumbers(3,4);
