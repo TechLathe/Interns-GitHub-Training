ee5580da4236ff4cc4aa66e6e67d7c5b17ac0929
Smiteshbhore21 <136716222+Smiteshbhore21@users.noreply.github.com>
2024-08-17 23:25:31 +0530

    Addition function code added.

--- Interns-Code/Main.js
+++ Interns-Code/Main.js
@@ -5,5 +5,11 @@ class Main {
         console.log("Hii. My name is Smitesh");
         
     }
+
+    static addNum(x,y) {
+        console.log("Addition of "+x+" and "+y+" is "+(x+y));
+    }
+
 }
 Main.printName();
+Main.addNum(3,4);
